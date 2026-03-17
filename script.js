3f26bdf1956dd33d9e813fb86636f75c65784c88
AdamPlaytested <165674839+AdamPlaytested@users.noreply.github.com>
2026-03-17 18:30:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let map = L.map("mymap").setView([41.219, -73.2423303], 15);
-let ourData = [];
+/* 
+Adam Aouassar, Sam Greenberg
+ITI 418
+Spring 2026
+script.js
+*/
 
+// Initiate Map
+let map = L.map("map").setView([41.219, -73.2423303], 15);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors",
   maxZoom: 20,
@@ -15,32 +21,6 @@ let iconOption = {
   iconSize: [30, 30],
 };
 let ourCustomIcon = L.icon(iconOption);
-
-fetch("./assets/location-data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    ourData = data;
-    for (let i = 0; i < data.length; i++) {
-      let option = document.createElement("option");
-      option.value = i + 1;
-      option.text = data[i].title;
-      document.querySelector(".select-dropdown").appendChild(option);
-
-      let marker = L.marker([data[i].latitude, data[i].longitude], {
-        icon: ourCustomIcon,
-      })
-        .bindPopup(
-          `<h3> ${data[i].title} </h3> 
-<p> ${data[i].description} </p>
-`
-        )
-        .on("click", () => {
-          map.flyTo([data[i].latitude, data[i].longitude], data[i].zoomLevel);
-        })
-        .addTo(map);
-    }
-  })
-  .catch((error) => alert(error));
 
 document.querySelector(".map-zoom-out-btn").addEventListener("click", () => {
   map.flyTo([41.215, -73.2423303], 14);
