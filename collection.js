11a219d8c4d9aef1ac9043d25f25a64fdf963d0a
AdamPlaytested <165674839+AdamPlaytested@users.noreply.github.com>
2026-04-27 19:37:18 -0400

    rendering spirits page

---
+++ collection.js
@@ -0,0 +1,16 @@
+/* 
+ Adam Aouassar, Sam Greenberg
+ ITI 418
+ Spring 2026
+ collection.js
+ */
+
+function render_spirits() {
+    document.getElementById("spirits")
+	.innerHTML +=
+	`<h3>
+	This is the text which has
+	been inserted by JS
+        </h3>`;
+};
+render_spirits();
