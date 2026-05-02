89205ad509d709472790be39e5c449368c01485d
AdamPlaytested <165674839+AdamPlaytested@users.noreply.github.com>
2026-05-02 17:03:45 -0400

    basic cookie reading

--- collection.js
+++ collection.js
@@ -5,12 +5,38 @@
  collection.js
  */
 
-function render_spirits() {
-    document.getElementById("spirits")
-	.innerHTML +=
-	`<h3>
-	This is the text which has
-	been inserted by JS
-        </h3>`;
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+async function render_spirits() {
+    const response = await fetch('locationList.csv');
+    const text = await response.text();
+    const render = document.getElementById("spirits");
+    
+    locations = Papa.parse(text, { header: true });
+    locations.data.forEach(function(row) {
+	if (getCookie(row.Page) === "1") {
+	    render.innerHTML +=
+	    `<h3>
+	    You havve visited ${row.Title}
+	    </h3>`;
+	}
+	
+    });
+    
 };
 render_spirits();
+
